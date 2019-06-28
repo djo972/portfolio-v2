9af5ba73319d5647529439055eb317e0b46b49b7
giovannypied <giovanny.pied@gmail.com>
2019-06-28 15:39:57 +0200

    add chat live

---
+++ live-chat-widget-js/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    instanceLocator: 'v1:us1:c57300a3-d8d9-4c63-9998-54122a518194',
+    key: '7b2c39f4-9ca9-4963-9fd1-683e1d7e0949:no2dfbWYjaRTbYkRSNgACc9mgIajYiZuLqzJNUI+TAQ='
+}
