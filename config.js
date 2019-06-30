f8e8eee7ac0b1ec4305256b655589b5332236dc9
djo_972 <giovanny.pied@gmail.com>
2019-06-30 21:28:52 +0200

    node version

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    instanceLocator: 'v1:us1:c0266633-91fb-4f9a-8567-34afe5f59db9',
+    key: '384b00e1-84c6-43f5-9152-6c717c2b092e:X1bpPSHhzyLSK5kamBaJW0ZVbhivUGTG35mHIBJpjK0='
+};
